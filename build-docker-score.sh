aa686cf591ff8c8a9dea04d4a6f28b31b2b2f51e
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-11-23 14:30:33 +0700

    fix build script

--- build-docker-score.sh
+++ build-docker-score.sh
@@ -2,8 +2,8 @@
 
 source config
 cd docker-score
-cp . ../docker-preprocess/preprocess.py
-cp . ../docker-train/keras.json
-cp . ../docker-train/.theanorc
+cp ../docker-preprocess/preprocess.py .
+cp ../docker-train/keras.json .
+cp ../docker-train/.theanorc .
 docker build -t ${scoreimage} .
 cd ..
