d76ec3666662a555e595604989cc7ea2db325569
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-11-23 12:08:55 +0700

    update scoring logging

--- docker-score/sc1_infer.py
+++ docker-score/sc1_infer.py
@@ -14,32 +14,38 @@ weights_file = sys.argv[5]
 predictions_file = sys.argv[6] if len(sys.argv) > 6 else PREDICTIONS_PATH
 
 # load model
+print('Loading model')
 with open(arch_file) as f:
     arch_json = f.read()
     model = model_from_json(arch_json)
-
 model.load_weights(weights_file)
 
 # predict images in crosswalk
+print('Predicting image by image')
 predictions = []
 with open(crosswalk_file, 'rb') as tsvin:
     crosswalk = csv.reader(tsvin, delimiter='\t')
     headers = next(crosswalk, None)
+    count = 1
     for row in crosswalk:
         dcm_subject_id = row[0]
         dcm_exam_id = row[1]
         dcm_laterality = row[4]
         dcm_filename = row[5]
         data = preprocess_image(os.path.join(dcm_dir, dcm_filename), EXPECTED_DIM[1], MAX_VALUE, FILTER_THRESHOLD)
-        prediction = model.predict(data, verbose=1)
-        predictions.append((dcm_subject_id, dcm_laterality, prediction[0][0]))
+        prediction = model.predict(data)
+        p = (dcm_subject_id, dcm_laterality, prediction[0][0])
+        print('{} {}'.format(count, p))
+        predictions.append(p)
+        count += 1
 
 # write predictions
+print('Writing to result {}'.format(predictions_file))
 with open(predictions_file, 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter='\t')
     header = ('subjectId', 'laterality', 'confidence')
     spamwriter.writerow(['subjectId', 'laterality', 'confidence'])
-    print(header)
     for p in predictions:
-        print(p)
         spamwriter.writerow(p)
+
+print('Done.')
