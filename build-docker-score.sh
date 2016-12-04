7658ad5ecdc29ca35ec26e553fb6b2cacde70528
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-12-04 18:11:33 +0700

    include vgg16 weights in scoring

--- build-docker-score.sh
+++ build-docker-score.sh
@@ -5,5 +5,6 @@ cd docker-score
 cp ../docker-preprocess/preprocess.py .
 cp ../docker-train/keras.json .
 cp ../docker-train/.theanorc .
+cp ../docker-train/vgg16_weights_th_dim_ordering_th_kernels_notop.h5 .
 docker build -t ${scoreimage} .
 cd ..
