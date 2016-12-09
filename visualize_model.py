6d1190f16d19276a19790ba741defa4b55887935
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-12-09 13:16:37 +0700

    add script to render model diagram to png

--- visualize_model.py
+++ visualize_model.py
@@ -3,11 +3,12 @@ from keras.applications.vgg16 import VGG16
 from keras.models import model_from_json
 import sys
 
-extractor = VGG16(weights='imagenet', include_top=False)
-plot(extractor, to_file='vgg16.png')
+# extractor = VGG16(weights='imagenet', include_top=False)
+# plot(extractor, to_file='vgg16.png')
 
 arch_file = sys.argv[1]
+output_file = sys.argv[2] if len(sys.argv) > 2 else 'model.png'
 with open(arch_file) as f:
     arch_json = f.read()
     model = model_from_json(arch_json)
-plot(model, to_file='model.png')
+plot(model, to_file=output_file)
