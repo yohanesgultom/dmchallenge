06686e8b5ff031e26fa9b3c1a87e026eac9f5695
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-12-09 11:41:36 +0700

    update sc2 scoring

--- docker-score/sc2_infer.py
+++ docker-score/sc2_infer.py
@@ -8,7 +8,7 @@ import sys
 import os
 import csv
 from keras.models import model_from_json
-from preprocess import preprocess_image, EXPECTED_DIM, MAX_VALUE, FILTER_THRESHOLD
+from preprocess import preprocess_image, metadata2numpy, normalize_meta, parse_int, parse_float, EXPECTED_DIM, MAX_VALUE, FILTER_THRESHOLD
 
 PREDICTIONS_PATH = 'predictions.tsv'
 
@@ -42,7 +42,30 @@ with open(crosswalk_file, 'rb') as tsvin:
         dcm_laterality = row[3]
         dcm_filename = row[4]
         data = preprocess_image(os.path.join(dcm_dir, dcm_filename), dcm_laterality)
-        prediction = model.predict(data)
+        meta = metadata2numpy({
+            'id': row[0],
+            'examIndex': row[1],
+            'daysSincePreviousExam': normalize_meta(row, 2, 'daysSincePreviousExam'),
+            'cancerL': parse_int(row[3]),
+            'cancerR': parse_int(row[4]),
+            'invL': parse_int(row[5]),
+            'invR': parse_int(row[6]),
+            'age': normalize_meta(row, 7, 'age'),
+            'implantEver': normalize_meta(row, 8, 'implantEver'),
+            'implantNow': normalize_meta(row, 9, 'implantNow'),
+            'bcHistory': normalize_meta(row, 10, 'bcHistory'),
+            'yearsSincePreviousBc': normalize_meta(row, 11, 'yearsSincePreviousBc'),
+            'previousBcLaterality': normalize_meta(row, 12, 'previousBcLaterality'),
+            'reduxHistory': normalize_meta(row, 13, 'reduxHistory'),
+            'reduxLaterality': normalize_meta(row, 14, 'reduxLaterality'),
+            'hrt': normalize_meta(row, 15, 'hrt'),
+            'antiestrogen': normalize_meta(row, 16, 'antiestrogen'),
+            'firstDegreeWithBc': normalize_meta(row, 17, 'firstDegreeWithBc'),
+            'firstDegreeWithBc50': normalize_meta(row, 18, 'firstDegreeWithBc50'),
+            'bmi': normalize_meta(row, 19, 'bmi'),
+            'race': normalize_meta(row, 20, 'race')
+        })
+        prediction = model.predict([data, meta])
         # collect predictions based on subjectId and laterality
         # to handle duplications
         key = '{}_{}'.format(dcm_subject_id, dcm_laterality)
