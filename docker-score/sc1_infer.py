26b561371f091d74e34e4fb4371af5238319fcbc
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-12-10 07:26:39 +0700

    try latest confidence for scoring

--- docker-score/sc1_infer.py
+++ docker-score/sc1_infer.py
@@ -70,8 +70,9 @@ with open(predictions_file, 'wb') as csvfile:
         pred = predictions[key]
         # aggregation strategy
         # confidence = sum(pred['p']) / float(len(pred['p']))  # average
-        confidence = max(pred['p'])  # max
-        # confidence = pred['p'][-1]  # latest
+        # confidence = max(pred['p'])  # max
+        # confidence = pred['p'][0]  # first
+        confidence = pred['p'][-1]  # latest
         row = (pred['id'], pred['lat'], confidence)
         spamwriter.writerow(row)
 
